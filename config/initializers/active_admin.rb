ea35766b69e37b70e38032543de4fe88f0546020
22a <meehanp2@tcd.ie>
2016-02-06 19:35:12 +0000

    added users, removed comments

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Ecdl Scripter"
+  config.site_title = "ECDL Scripter"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -266,4 +266,10 @@ ActiveAdmin.setup do |config|
   # of those filters by default here.
   #
   # config.include_default_association_filters = true
+  
+  # Disable Comments
+  ActiveAdmin.setup do |config|
+    config.comments = false
+  end
+
 end
