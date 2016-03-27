205edf40ff2df0c3dfcc2fbc0b30ac4e18352d9f
22a <meehanp2@tcd.ie>
2016-03-27 21:18:16 +0100

    question specification in syllabus item form

--- app/admin/syllabus_item.rb
+++ app/admin/syllabus_item.rb
@@ -14,4 +14,26 @@ ActiveAdmin.register SyllabusItem do
 # end
 
 
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :name
+      f.has_many :questions, heading: 'Questions', allow_destroy: false, new_record: false do |q, i|
+        q.input :task_item, label: i.to_s() + '/80 Task Item:'
+        q.input :practical
+      end
+    end
+    f.actions
+  end
+
+  controller do
+    def permitted_params
+      params.permit syllabus_item: [ :name, questions_attributes: [:task_item, :practical] ]
+    end
+    def new
+      @syllabus_item = SyllabusItem.new
+      80.times { @syllabus_item.questions.build }
+      @syllabus_item
+    end
+  end
 end
--- app/models/syllabus_item.rb
+++ app/models/syllabus_item.rb
@@ -1,6 +1,5 @@
 class SyllabusItem < ActiveRecord::Base
   has_many :questions
   accepts_nested_attributes_for :questions,
-    :allow_destroy => true,
     :reject_if     => :all_blank
 end
