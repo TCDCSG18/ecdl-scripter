d79fc868fb23af26ee4588d5e48ca1bd10d9a2fe
22a <meehanp2@tcd.ie>
2016-04-01 15:47:25 +0100

    naive docx styling

--- app/controllers/test_scripts_controller.rb
+++ app/controllers/test_scripts_controller.rb
@@ -2,6 +2,9 @@ class TestScriptsController < InheritedResources::Base
 
   def show
     @test_script = TestScript.find(params[:id])
+    @questions = @test_script.syllabus_item.questions
+      .where("belongs_to_test_#{@test_script.syllabus_item.test_scripts.index(@test_script)}" => true)
+      .shuffle
     respond_to do |format|
       format.docx { headers["Content-Disposition"] = "attachment; filename=\"" + @test_script.name + ".docx\""  }
     end
